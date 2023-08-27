0c74d710d8100e3e40c305811ba46d55145289ec
caiquegl <arq.caique@hotmail.com>
2023-08-27 20:42:02 -0300

    api

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: process.env.url,
+    baseURL: `${process.env.url}/api`,
     headers: {
         'Content-Type': 'application/json',
     }
